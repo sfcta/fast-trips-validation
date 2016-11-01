d54cc31993b301a87974d277f0d6dc53e0faf242
Lisa Zorn <lzorn@mtc.ca.gov>
2016-10-31 17:11:34 -0700

    Use the input from box and run map script

--- scripts/processOnBoardSurvey.bat
+++ scripts/processOnBoardSurvey.bat
@@ -5,11 +5,13 @@
 
 :: These are Lisa's settings
 if %USERNAME%==lzorn (
-  set OBS_RAW_FILE=M:\Data\OnBoard\Data and Reports\_data Standardized\share_data\survey.csv
+  rem set OBS_RAW_FILE=M:\Data\OnBoard\Data and Reports\_data Standardized\share_data\survey.csv
+  set OBS_RAW_FILE=C:\Users\lzorn\Box Sync\SHRP C-10\3-Transit Demand\OBS_fasttrips_demand\OBSdata_wBART.csv
   set MTCMAZ_TO_SFTAZ_FILE=C:\Users\lzorn\Box Sync\SHRP C-10\4-Transit Rider Behavior\mtcmaz_to_sftaz.csv
   set OBS_GTFS_ROUTE_FILE=C:\Users\lzorn\Box Sync\SHRP C-10\4-Transit Rider Behavior\OBS_GTFS_route_dict.xlsx
   set NETWORK_DIR=C:\Users\lzorn\Box Sync\SHRP C-10\2-Network Supply\sfcta\network_draft1.9
   set CODE_DIR=C:\Users\lzorn\Documents\fast-trips-validation
+  set FT_CODE_DIR=C:\Users\lzorn\Documents\fast-trips-develop
 )
 
 :: copy the raw On Board Survey data file into place
@@ -26,19 +28,33 @@ copy "%OBS_GTFS_ROUTE_FILE%" .
 ::   Reads: OBSdata_wBART.csv, mtcmaz_to_sftaz.csv
 ::  Writes: OBSdata_wBART_wSFtaz.csv
 python "%CODE_DIR%\scripts\OBS_to_DynoDemand\MTCmaz_to_SFtaz\add_SFtaz_to_OBS.py"
+if errorlevel 1 goto error
 
 :: Convert the on board survey data into the fasttrips input format (Dyno-Demand)
 ::   Reads: OBSdata_wBART_wSFtaz.csv, DepartureTimeCDFs.dat
 ::  Writes: household.txt, person.txt, trip_list.txt
 python "%CODE_DIR%\scripts\OBS_to_DynoDemand\OBS_to_DynoDemand.py"
+if errorlevel 1 goto error
 
 :: ===================== fasttrips ouptput (Dyno-Path) ==================================================================
 :: Add the stop ID
 ::  Reads: OBSdata_wBART_wSFtaz.csv, stops.txt
 :: Writes: OBSdata_wBART_wSFtaz_wStops.csv
+set PYTHONPATH=%FT_CODE_DIR%
 python "%CODE_DIR%\scripts\OBS_to_FToutput\Add_StopID_OBS\add_StopID_OBS.py" "%NETWORK_DIR%"
+if errorlevel 1 goto error
 
 :: Create the dyno-path version of the On Board Survey
 ::  Reads: trip_list.txt, OBSdata_wBART_wSFtaz_wStops.csv
-:: Writes: OBS_FToutput_links.csv
+:: Writes: OBS_FToutput_links.csv, OBS_FToutput_paths.csv
 python "%CODE_DIR%\scripts\OBS_to_FToutput\OBS_to_FToutput.py"
+if errorlevel 1 goto error
+
+:: ===================== map the fasttrips output ==================================================================
+:map
+copy OBS_FToutput_links.csv pathset_links.csv
+copy OBS_FToutput_paths.csv pathset_paths.csv
+python "%FT_CODE_DIR%\scripts\create_tableau_path_map.py" "%NETWORK_DIR%" .
+if errorlevel 1 goto error
+
+:error
