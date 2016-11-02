d80446c9c90eafb8296e874a816aa8aa913f09c0
Andisheh Ranjbari <ARanjbari@psrc.org>
2016-11-02 16:59:25 -0700

    Fix time_target issue.

--- scripts/OBS_to_DynoDemand/OBS_to_DynoDemand.py
+++ scripts/OBS_to_DynoDemand/OBS_to_DynoDemand.py
@@ -33,6 +33,48 @@ pass_media = ['Clipper','clipper','pass','exempt']
 
 AccEgrs_dict = {'pnr':'PNR', 'knr':'KNR', '.':'walk'}
 
+#MTC defined time periods for survey
+survey_time_period = {3 :'EARLY AM',
+                      4 :'EARLY AM',
+                      5 :'EARLY AM',
+                      6 :'AM PEAK',
+                      7 :'AM PEAK',
+                      8 :'AM PEAK',
+                      9 :'AM PEAK',
+                      10:'MIDDAY',
+                      11:'MIDDAY',
+                      12:'MIDDAY',
+                      13:'MIDDAY',
+                      14:'MIDDAY',
+                      15:'PM PEAK',
+                      16:'PM PEAK',
+                      17:'PM PEAK',
+                      18:'PM PEAK',
+                      19:'EVENING',
+                      20:'EVENING',
+                      21:'NIGHT',
+                      22:'NIGHT',
+                      23:'NIGHT',
+                      0 :'NIGHT',
+                      1 :'NIGHT',
+                      2 :'NIGHT'}
+
+hours = {'Before 6 a.m.'       : None,
+         '6 - 6:59 a.m.'       : 6,
+         '7 - 7:59 a.m.'       : 7,
+         '8 - 8:59 a.m.'       : 8,
+         '9 - 9:59 a.m.'       : 9,
+        '10 - 10:59 a.m.'      : 10,
+        '11 a.m. - 11:59 a.m.' : 11,
+        '12 - 12:59 p.m.'      : 12,
+        '1 - 1:59 p.m.'        : 13,
+        '2 - 2:59 p.m.'        : 14,
+        '3 - 3:59 p.m.'        : 15,
+        '4 - 4:59 p.m.'        : 16,
+        '5 - 5:59 p.m.'        : 17,
+        '6 - 6:59 p.m.'        : 18,
+        '7 - 7:59 p.m.'        : 19,
+        'After 8 p.m.'         : None}
 #############################################################################################################
 dep_time_dist = readDistributionCDFs(os.path.join(os.path.dirname(os.path.realpath(__file__)), "DepartureTimeCDFs.dat"))
 
@@ -110,19 +152,83 @@ df["egress_mode"] = df["egress_mode"].replace(AccEgrs_dict)
 df["mode"] = df["access_mode"] + "-transit-" + df["egress_mode"]
 
 #### create departure and arrival time based on depart_hour and return_hour
-# create departure column = departure time in minutes after midnight
+# create departure/arrival column = departure/arrival time in minutes after midnight
 df["departure"]      = df["depart_hour"].map(lambda x: chooseTimeFromDistribution(dep_time_dist[x]), na_action='ignore')
 df["departure_time"] = df["departure"].map(lambda x: convertTripTime(x), na_action='ignore')
 df["arrival"]        = df["return_hour"].map(lambda x: chooseTimeFromDistribution(dep_time_dist[x]), na_action='ignore')
 df["arrival_time"]   = df["arrival"].map(lambda x: convertTripTime(x), na_action='ignore')
-df["time_target"]    = "departure"           # don't know
+#df["time_target"]    = "departure"           # don't know
+
+#### create time_target
+# create survey_start_hr
+# BART
+BART = pd.read_csv('BART_TimeStartedSurvey.csv',
+                    na_values=['Missing - Question Not Asked','Missing - Dummy Record'])
+BART['Unique_ID'] = BART['ID'].astype(str) + '---BART---2015'
+BART['survey_start_hr'] = BART['Q1_START'].astype(str).str.split(' ').str.get(1).str.split(':').str.get(0)
+BART = BART[['Unique_ID','survey_start_hr']]
+# Caltrain
+Caltrain = pd.read_csv('Caltrain_TimeStartedSurvey.csv')
+Caltrain['Unique_ID'] = Caltrain['ID'].astype(str) + '---Caltrain---2014'
+Caltrain = Caltrain [Caltrain['TIME SURVEY BEGAN'].notnull()]
+Caltrain['survey_start_hr'] = Caltrain['TIME SURVEY BEGAN'].astype(str).str.split(':').str.get(0).astype(int)
+Caltrain['tp'] = Caltrain['TIME SURVEY BEGAN'].astype(str).str.split(' ').str.get(1)
+Caltrain.loc[ (Caltrain['tp']!='pm')&(Caltrain['survey_start_hr']!=12), 'survey_start_hr'] = Caltrain['survey_start_hr']+12
+Caltrain = Caltrain[['Unique_ID','survey_start_hr']]
+# NapaVine
+NapaVine = pd.read_csv('NapaVINE_TimeStartedSurvey.csv')
+NapaVine['Unique_ID'] = NapaVine['ID'].astype(str) + '---Napa Vine---2014'
+NapaVine['survey_start_hr'] = NapaVine['TIME_BOARDED'].replace(hours)
+NapaVine = NapaVine[['Unique_ID','survey_start_hr']]
+# TriDelta
+TriDelta = pd.read_csv('TriDelta_TimeStartedSurvey.csv')
+TriDelta['Unique_ID'] = TriDelta['ID'].astype(str) + '---Tri-Delta---2014'
+TriDelta['survey_start_hr'] = TriDelta['TIME_BOARDED'].replace(hours)
+TriDelta = TriDelta[['Unique_ID','survey_start_hr']]
+# put them all together and merge with df
+survey_start_hr_df = pd.concat([BART, Caltrain, NapaVine, TriDelta], axis=0)
+survey_start_hr_df = survey_start_hr_df.loc[ pd.notnull(survey_start_hr_df['survey_start_hr']) ]
+df = pd.merge(df,survey_start_hr_df,how='left',on='Unique_ID')
+
+# create dep_hr and arr_hr as integer, and put 99 for NaN
+df['dep_hr'] = df['depart_hour'].fillna(99).astype(int)
+df['arr_hr'] = df['return_hour'].fillna(99).astype(int)
+df['survey_start_hr'] = df['survey_start_hr'].fillna(99).astype(int)
+# set time_target based on survey_time, and allow for up to 3 hour deviations, b/c
+# it's not unreasanable if e.g. one departed home at 6:55 (depart_hr=6), and
+# being surveyed at 9:03 (survey_start_hr=9).
+df['time_target'] = None
+hr_diff = 0
+while (hr_diff <=3):
+    df.loc[ (df['dep_hr']==df['survey_start_hr'])&(df['time_target'].isnull())&(df['dep_hr']<90), 'time_target'] = 'departure'
+    df.loc[ (df['arr_hr']==df['survey_start_hr'])&(df['time_target'].isnull())&(df['arr_hr']<90), 'time_target'] = 'arrival'
+    print "set time_target for", len(df[df['time_target'].notnull()]), "trips, with hr_diff=", hr_diff
+    hr_diff = hr_diff+1
+    # continue only for those with null time_target to avoid overwriting
+    df.loc[ df['time_target'].isnull(), 'dep_hr'] = df['dep_hr']+hr_diff
+    df.loc[ df['time_target'].isnull(), 'arr_hr'] = df['arr_hr']-hr_diff
+# For those without survey_start_hr, try the same process using time periods
+df['dep_hr'] = df['depart_hour'].fillna(99).astype(int)
+df['arr_hr'] = df['return_hour'].fillna(99).astype(int)
+df['dep_tp'] = df['dep_hr'].replace(survey_time_period)
+df['arr_tp'] = df['arr_hr'].replace(survey_time_period)
+hr_diff = 0
+while (hr_diff <=3):
+    df.loc[ (df['survey_start_hr']>90)&(df['dep_tp']==df['day_part'])&(df['time_target'].isnull()), 'time_target'] = 'departure'
+    df.loc[ (df['survey_start_hr']>90)&(df['arr_tp']==df['day_part'])&(df['time_target'].isnull()), 'time_target'] = 'arrival'
+    print "set time_target for", len(df[df['time_target'].notnull()]), "trips, using time period with hr_diff=", hr_diff
+    hr_diff = hr_diff+1
+    df.loc[ df['time_target'].isnull(), 'dep_tp'] = (df['dep_hr']+hr_diff).replace(survey_time_period)
+    df.loc[ df['time_target'].isnull(), 'arr_tp'] = (df['arr_hr']-hr_diff).replace(survey_time_period)
+# For now, ssume "departure" for those we don't know
+df.loc[ df['time_target'].isnull(), "time_target" ] = "departure"
 
 #### Value of Time
 '''VoT based on SFCTA RPM-9 Report, p39:
 - non-work VoT = 2/3 work VoT,
 - Impose a minimum of $1/hr and a maximum of $50/hr,
 - Impose a maximum of $5/hr for workers younger than 18 years old.'''
-df["vot"] = -1.0   # define it as an float type
+df["vot"] = -1.0   # define it as float type
 df.loc[ (df["age"]<=18)&(df["worker_status"]=='full-time'),            "vot"] = 5.0  # max $5 for workers under than 18 years old
 df.loc[ pd.isnull(df["hh_income"])|(df["worker_status"]!='full-time'), "vot"] = 1.0  # $1 for non-workers
 
